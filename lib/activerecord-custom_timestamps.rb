1147666de539343ff69256ac5904a8a99db3fbee
Gary S. Weaver <garysweaver@gmail.com>
2012-08-22 17:08:55 -0400

    removing puts of version on load

--- lib/activerecord-custom_timestamps.rb
+++ lib/activerecord-custom_timestamps.rb
@@ -1,5 +1,3 @@
 require 'activerecord-custom_timestamps/version'
 require 'activerecord-custom_timestamps/model'
 require 'activerecord-custom_timestamps/railtie' if defined?(Rails)
-
-puts "CustomTimestamps #{CustomTimestamps::VERSION}"
